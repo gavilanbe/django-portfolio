e7879f5c28f35cf0530b23da736e376eaea281da
gavilanbe <nahuel@gavilanbe.io>
2023-05-22 23:16:16 +0200

    Configured email backend for local development

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -127,3 +127,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Console email backend 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
