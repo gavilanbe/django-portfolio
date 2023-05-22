c0f148763518010580298ae9cf4c3fa974cce521
gavilanbe <nahuel@gavilanbe.io>
2023-05-22 19:14:43 +0200

    Created views templates for contantc and about

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Project
 
-def home(request): 
-    return render(request, 'portfolio/home.html')
+def home(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolio/home.html', {'projects': projects})
+
+def about(request):
+    return render(request, 'portfolio/about.html')
+
+def contact(request):
+    return render(request, 'portfolio/contact.html')
